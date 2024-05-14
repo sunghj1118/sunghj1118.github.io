3764362ec29818bba1e3a62de2c7101bef8c0c69
sunghj1118 <sunghj1118@gmail.com>
2024-05-14 12:28:42 +0900

    feat: set up basic structure to render md files and images

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const contentDiv = document.getElementById('content');
-    const postFilename = 'first_post.md'; // For now, we'll just load the welcome.md file
+    const postFilename = 'welcome.md'; // For now, we'll just load the welcome.md file
 
-    fetch(`/posts/${postFilename}`)
-        .then(response => response.text())
+    fetch(`posts/${postFilename}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(markdown => {
-            contentDiv.innerHTML = marked(markdown);
+            console.log(marked); // Check if marked is correctly loaded
+            if (typeof marked === 'function') {
+                contentDiv.innerHTML = marked(markdown);
+            } else {
+                console.error('marked.js is not loaded or not a function');
+                contentDiv.innerHTML = '<p>Sorry, the content could not be processed.</p>';
+            }
         })
         .catch(error => {
             console.error('Error fetching the markdown file:', error);
--- posts/first_post.md
+++ posts/first_post.md
@@ -5,7 +5,9 @@ Date: 2024-05-14
 Welcome to my blog! Here, I'll share my journey as a developer.  
 Stay tuned for more posts about programming, technology, and more.
 
-![Welcome Image](../images/welcome.jpeg)
+![Welcome](/images/welcome.jpeg)
+<!-- ![Welcome](/Users/hyunjoon/Projects/Personal/sunghj1118.github.io/images/welcome.jpeg) -->
+
 
 ## Topics
 - CS
