0a097c58bdc07200847e9d206dc59c3139794413
sunghj1118 <sunghj1118@gmail.com>
2024-06-27 09:58:17 +0900

    feat: add Graph tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -247,7 +247,7 @@ const IndexPage = ({ data }) => {
   const tagsWithSubTags = {
     Blog: ['Gatsby', 'NPM', 'Github'],
     Infra: ['Kubernetes', 'Docker', 'Container'],
-    Algorithm: ['Theory', 'Log', 'DP', 'Heap', 'Hashmap', 'Sorting', 'Two Pointer', "Bit", "Tree", "Linked List"],
+    Algorithm: ['Theory', 'Log', 'DP', 'Heap', 'Hashmap', 'Sorting', 'Two Pointer', "Bit", "Tree", "Linked List", "Graph"],
     CS: ['Architecture'],
     AI: []
   };
