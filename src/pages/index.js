6343db835344cd5108549f671fdeb1abe7cc738c
sunghj1118 <sunghj1118@gmail.com>
2024-06-29 23:25:59 +0900

    feat: add 'Greedy' tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -247,7 +247,7 @@ const IndexPage = ({ data }) => {
   const tagsWithSubTags = {
     Blog: ['Gatsby', 'NPM', 'Github'],
     Infra: ['Kubernetes', 'Docker', 'Container'],
-    Algorithm: ['Theory', 'Log', 'DP', 'Heap', 'Hashmap', 'Sorting', 'Two Pointer', "Bit", "Tree", "Linked List", "Graph", "Sliding Window"],
+    Algorithm: ['Theory', 'Log', 'DP', 'Heap', 'Hashmap', 'Sorting', 'Two Pointer', "Bit", "Tree", "Linked List", "Graph", "Sliding Window", "Greedy"],
     CS: ['Architecture'],
     AI: []
   };
